37af0f9a670442cc8e980d7e941afefae6f1a1b6
Chukwuemeka Kennedy <princeukaga@gmail.com>
2024-08-29 22:05:54 +0100

    homepage ui done

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:carma_app/pages/auth/loginPage.dart';
 import 'package:carma_app/pages/auth/signupPage.dart';
+import 'package:carma_app/pages/home/homePage.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -14,7 +15,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: SignUpPage(),
+      home: HomePage(),
     );
   }
 }
---
+++ lib/pages/home/homePage.dart
@@ -0,0 +1,173 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: const Color.fromARGB(255, 243, 148, 30),
+      ),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const FaIcon(
+                    FontAwesomeIcons.solidBell,
+                    color: Color.fromARGB(255, 243, 148, 30),
+                  ),
+                  Image.asset('assets/images/carmalogo.png')
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    children: [
+                      const CircleAvatar(
+                        radius: 40,
+                        backgroundImage:
+                            AssetImage('assets/images/profile.png'),
+                      ),
+                      Text(
+                        'Hi, JOHN',
+                        style: GoogleFonts.openSans(
+                          fontSize: 17,
+                          color: const Color.fromARGB(255, 243, 148, 30),
+                        ),
+                      )
+                    ],
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      Image.asset('assets/images/chat.png'),
+                      Row(
+                        children: [
+                          Image.asset('assets/images/speaker.png'),
+                          Text(
+                            'Popular Subscriptions',
+                            style: GoogleFonts.openSans(
+                              fontSize: 17,
+                              color: Colors.red,
+                            ),
+                          )
+                        ],
+                      )
+                    ],
+                  )
+                ],
+              ),
+              const SizedBox(height: 10),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Image.asset('assets/images/car1.png'),
+                  Image.asset('assets/images/car2.png'),
+                  Image.asset('assets/images/car3.png'),
+                ],
+              ),
+              const SizedBox(height: 20),
+              Card(
+                elevation: 4,
+                child: Container(
+                  height: 167,
+                  width: 345,
+                  decoration: BoxDecoration(
+                      color: const Color.fromARGB(255, 232, 228, 228),
+                      borderRadius: BorderRadius.circular(15)),
+                  child: Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Image.asset('assets/images/garage.png'),
+                            const Icon(
+                              Icons.arrow_forward_ios_rounded,
+                              color: Color.fromARGB(255, 149, 149, 149),
+                            )
+                          ],
+                        ),
+                        Text(
+                          'Garage',
+                          style: GoogleFonts.openSans(
+                            fontSize: 17,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        Text(
+                          'Hire someone to do the job for you',
+                          style: GoogleFonts.openSans(
+                              fontSize: 14,
+                              fontWeight: FontWeight.w700,
+                              color: const Color.fromARGB(255, 0, 0, 0)
+                                  .withOpacity(0.4)),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 20),
+              Card(
+                elevation: 4,
+                child: Container(
+                  height: 167,
+                  width: 345,
+                  decoration: BoxDecoration(
+                      color: const Color.fromARGB(255, 232, 228, 228),
+                      borderRadius: BorderRadius.circular(15)),
+                  child: Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Image.asset('assets/images/service.png'),
+                            const Icon(
+                              Icons.arrow_forward_ios_rounded,
+                              color: Color.fromARGB(255, 149, 149, 149),
+                            )
+                          ],
+                        ),
+                        Text(
+                          'Service',
+                          style: GoogleFonts.openSans(
+                            fontSize: 17,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        Text(
+                          'What would you like to get done',
+                          style: GoogleFonts.openSans(
+                              fontSize: 14,
+                              fontWeight: FontWeight.w700,
+                              color: const Color.fromARGB(255, 0, 0, 0)
+                                  .withOpacity(0.4)),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
